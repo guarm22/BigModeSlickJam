5fdb25ceb4b3ad6a8c4756b6c7a8f9e308709ce0
guarm22 <60443541+guarm22@users.noreply.github.com>
2026-02-07 23:03:18 -0500

    endgame

--- Assets/Scripts/PlayerUI.cs
+++ Assets/Scripts/PlayerUI.cs
@@ -1,5 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerUI : MonoBehaviour {
     public GameObject pauseMenu;
@@ -8,13 +11,41 @@ public class PlayerUI : MonoBehaviour {
 
     public static PlayerUI instance;
 
+    private bool endGame = false;
     public GameObject endGameScreen;
+    public TMP_Text highscore;
+    public TMP_Text thisScore;
+
+
+    public GameObject egTimesUp;
+
+    public GameObject egRest;
 
     void Start() {
         instance = this;
         paused = false;        
     }
 
+    public void EndGameScreen() {
+        egTimesUp.SetActive(true);
+        egRest.SetActive(false);
+        endGameScreen.SetActive(true);
+        StartCoroutine(EndGameScreenPart2());
+    }
+
+    private IEnumerator EndGameScreenPart2() {
+        yield return new WaitForSeconds(2f);
+        egTimesUp.SetActive(false);
+        egRest.SetActive(true);
+        endGame = true;
+        paused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+        highscore.text = PlayerPrefs.GetFloat("HighScore").ToString();
+        thisScore.text = TaskManager.Instance.CalculateCompletionPercent().ToString();
+    }
+
+ 
     private void Pause() {
         pauseMenu.SetActive(true);
         paused = true;
@@ -31,6 +62,11 @@ public class PlayerUI : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+        if(Input.GetKeyDown(KeyCode.R) && endGame) {
+            SceneManager.LoadScene(1);
+        }
+        if(endGame) {return;}
         if(Input.GetKeyDown(KeyCode.Escape)) {
             paused = !paused;
             if(paused) {Pause();}
